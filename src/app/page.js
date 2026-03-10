d29524b2ef6771a020dd938549066947d231f9e6
Deepak Suman <deepak.suman@cpuniverse.in>
2026-03-10 17:59:53 +0530

    Initial Next.js project

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,9 @@
-import Image from "next/image";
+import Home from "./components/Home";
 
-export default function Home() {
+export default function Page() {
   return (
-<></>
+    <div>
+      <Home />
+    </div>
   );
 }
